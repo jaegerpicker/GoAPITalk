86e10d3efa422938df87a1b12dbb5ce822a04012
Shawn Campbell <scampbell@cashstar.com>
2014-11-06 19:43:59 -0500

    Rails finished up

--- ror_example/rails_example/app/controllers/api/todos_controller.rb
+++ ror_example/rails_example/app/controllers/api/todos_controller.rb
@@ -1,14 +1,50 @@
-odule Api
-  class TodosController < Api::BaseController
-
-    private
-
-      def todo_params
-        params.require(:todo).permit(:task)
+module Api
+  class TodosController < ApplicationController
+      def index
+          render json: Todo.all
       end
-
-      def query_params
-        params.permit(:task)
+      def create
+          print "Json is: "
+          print params
+          @u = User.find_by_id(params['user_id'])
+          if @u == nil
+              render json: {message: "User not found"}
+          end
+          @t = Todo.new
+          @t.task = params['task']
+          @t.user = @u
+          if @t.save
+              render json: @t
+          else
+              render nothing: true, status: :bad_request
+          end
+      end
+      def show
+          print params['id']
+          @t = Todo.find_by_id(params['id'])
+          render json: @t
+      end
+      def new
+          create()
+      end
+      def destroy
+          @t = Todo.find_by_id(params['id'])
+          @t.destroy()
+          render json: {message: "todo deleted"}
+      end
+      def edit
+          @t = Todo.find_by_id(params['id'])
+          @u = User.find_by_id(params['user_id'])
+          @t.task = params['task']
+          @t.user = @u
+          if @t.save
+              render json: @t
+          else
+              render nothing: true, status: :bad_request
+          end
+      end
+      def update
+          edit()
       end
 
   end
